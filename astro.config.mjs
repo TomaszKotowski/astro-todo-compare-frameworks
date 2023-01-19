274061d131dfd732be68f6f9178bded72fc338f1
Tomasz Kotowski <t.kotowski.2@gmail.com>
2023-01-19 18:31:25 +0100

    add frameworks and create placeholder pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+import vue from "@astrojs/vue";
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), svelte(), vue(), solidJs()]
 });
